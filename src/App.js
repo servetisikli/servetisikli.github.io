6ccd4c11c1762b633803c41ab32293f6eefe5da8
servet-isikli <servetisikli@yahoo.com>
2023-07-05 00:14:13 +0200

    assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Carousels from "./components/Carousels";
 import Header from "./components/Header";
+import ProjectCard from "./components/ProjectCard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousels />
+      <Home />
+      <ProjectCard />
     </div>
   );
 }
