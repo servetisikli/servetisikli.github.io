13a5fba49cbd98915ae61224ac1bf7615b2daf44
servet-isikli <servetisikli@yahoo.com>
2023-07-13 21:44:01 +0200

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,14 @@ function Header() {
 
   return (
     <div>
-      <Navbar bg="dark" data-bs-theme="dark" >
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">{user.name}</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#About">About</Nav.Link>
             <Nav.Link href="projects">Projects</Nav.Link>
+            <Nav.Link href="#test">Test</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
